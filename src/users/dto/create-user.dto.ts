ed76640b2baf69a98ffdb19715b34c1e1ab22cf9
Thales_edu <thalesdev22@gmail.com>
2021-10-23 08:18:36 -0300

    add class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString({ message: 'Nome é do tipo string' })
+  @IsNotEmpty({ message: 'password é obrigatório' })
   name: string;
+
+  @IsString({ message: 'Email é do tipo string' })
+  @IsNotEmpty({ message: 'password é obrigatório' })
   email: string;
+
+  @IsString({ message: 'password é do tipo string' })
+  @IsNotEmpty({ message: 'password é obrigatório' })
   password: string;
 }
