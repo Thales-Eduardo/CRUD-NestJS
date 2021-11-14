49fad2a8d56cd7fff068c5db4c503d0e8e9bbc8e
Thales Eduardo <thalesdev22@gmail.com>
2021-11-14 11:19:29 -0300

    Atualizando tratamento de erro.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AuthUserDto } from './dto/auth-user.sto';
@@ -14,13 +14,13 @@ export class UsersService {
     const findByEmail = await this.UserModel.findOne({ email: email });
 
     if (!findByEmail) {
-      throw new Error('E-mail ou senha esta incorreto.');
+      throw new HttpException('E-mail ou senha esta incorreto.', HttpStatus.NOT_FOUND);
     }
 
     const findByPassword = await this.UserModel.findOne({ password: password });
 
     if (!findByPassword) {
-      throw new Error('E-mail ou senha esta incorreto.');
+      throw new HttpException('E-mail ou senha esta incorreto.', HttpStatus.NOT_FOUND);
     }
   }
 
@@ -28,7 +28,7 @@ export class UsersService {
     const email = await this.UserModel.findOne({ email: createUserDto.email });
 
     if (email) {
-      throw new Error('Esse e-mail já esta cadastrado.');
+      throw new HttpException('Esse e-mail já esta cadastrado.', HttpStatus.BAD_REQUEST);
     }
 
     const user = new this.UserModel(createUserDto);
@@ -40,7 +40,11 @@ export class UsersService {
   }
 
   public async findOne(id: string): Promise<User | undefined> {
-    return this.UserModel.findById(id);
+    const user = await this.UserModel.findById(id);
+    if(!user){
+      throw new HttpException('Usuário não encontrado.', HttpStatus.NOT_FOUND);
+    }
+    return user;
   }
 
   public async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
